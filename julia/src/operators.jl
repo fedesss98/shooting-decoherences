730cfddc4f5210a6d2fa24ef226055c172aec320
Federico Amato <federico.amato01@unipa.it>
2026-02-19 16:38:39 +0100

    Add references for channels

--- julia/src/operators.jl
+++ julia/src/operators.jl
@@ -30,6 +30,7 @@ end
     get_amplitudedamping_operators(gamma, t)
 See for example
     https://www.preskill.caltech.edu/ph219/chap3_15.pdf#page=24.11
+    https://docs.pennylane.ai/en/stable/code/api/pennylane.AmplitudeDamping.html
 """
 function get_amplitudedamping_operators(gamma, t)
     e = exp(-gamma*t)
@@ -46,6 +47,8 @@ end
 
 """
     get_phasedamping_operators(gamma, t)
+See for example
+    https://docs.pennylane.ai/en/stable/code/api/pennylane.PhaseDamping.html
 """
 function get_phasedamping_operators(gamma, t)
     e = exp(-gamma*t)
@@ -76,6 +79,8 @@ end
 
 """
     get_bitflip_operators(gamma, t)
+See for example
+    https://docs.pennylane.ai/en/stable/code/api/pennylane.BitFlip.html
 """
 function get_bitflip_operators(gamma, t)
     p = (1.0 - exp(-gamma*t)) / 2.0
