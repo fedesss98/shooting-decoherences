16f60f1a3407386f84c9c79602292fbd5da63663
Federico Amato <federico.amato01@unipa.it>
2026-06-16 06:36:10 +0200

    Export new functions

--- julia/src/PetzMaps.jl
+++ julia/src/PetzMaps.jl
@@ -3,17 +3,23 @@ module PetzMaps
 using LinearAlgebra
 using Random
 
-export 
-get_depolarizing_operators, 
-get_amplitudedamping_operators, 
-get_bitflip_operators, 
-get_phasedamping_operators,
-get_random_operators,
-# initial states
-codespace_state, codespace_dm, input_state, thermal_state, random_state,
-apply_channel, apply_extended_channel, recovery_map, rand_state_with_spectrum, expand_kraus_operators,
-# metrics
-fidelity, overlap
+export
+    get_depolarizing_operators,
+    get_amplitudedamping_operators,
+    get_bitflip_operators,
+    get_phasedamping_operators,
+    get_random_operators,
+    get_pin_operators,
+    correlate_kraus_operators,
+    tensor_power,
+    normalize_kraus_operators,
+    # initial states
+    codespace_state, codespace_dm, input_state, thermal_state, random_state,
+    thermal_state_hopping,
+    single_excitation_dm,
+    apply_channel, apply_extended_channel, recovery_map, rand_state_with_spectrum, expand_kraus_operators,
+    # metrics
+    fidelity, overlap
 
 include("utils.jl")
 include("operators.jl")
