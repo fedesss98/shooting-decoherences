e0c7a0702a78fb099480129ecd25a7bcdf3c4507
Federico Amato <federico.amato01@unipa.it>
2026-06-24 17:21:08 +0200

    Correct for finding old results

--- DecoKiller/src/adaptive_recovery.jl
+++ DecoKiller/src/adaptive_recovery.jl
@@ -216,9 +216,12 @@ function iterate_recovery!(
   C2 = collapse_map(ptrace_ancilla(rho2_, ds, da), rho2; pin=config.pin)
 
   if config.recover_all
-    Nx = Cx * Xi * Nx
-    N1 = C1 * Xi1 * N1
-    N2 = C2 * Xi2 * N2
+    # Nx = Cx * Xi * Nx
+    # N1 = C1 * Xi1 * N1
+    # N2 = C2 * Xi2 * N2
+    Nx = Xi * Nx
+    N1 = Xi1 * N1
+    N2 = Xi2 * N2
   end
 
   # ======================================
@@ -248,10 +251,10 @@ function iterate_recovery!(
   else
     state.ρ_codespace0
   end
-  if step == 1 || step == config.n_timesteps
-    @debug "Rho initial before projection:\n$rho_initial" rho_initial
-    @debug "Rho recovered before projection:\n$rho_rec" rho_rec
-  end
+  # if step == 1 || step == config.n_timesteps
+  #   @debug "Rho initial before projection:\n$rho_initial" rho_initial
+  #   @debug "Rho recovered before projection:\n$rho_rec" rho_rec
+  # end
   if config.n_qubits > 1
     rho_rec, prob_codespace = project_to_codespace(
       rho_rec, config.n_qubits;
@@ -269,11 +272,11 @@ function iterate_recovery!(
       projection=config.codespace_projection
     )
   end
-  if step == 1 || step == config.n_timesteps
-    @debug "Rho initial after projection:\n$rho_initial"
-    @debug "Recovered rho:\n$rho_rec" rho_rec
-    # @debug "Free rho" rho_free
-  end
+  # if step == 1 || step == config.n_timesteps
+  #   @debug "Rho initial after projection:\n$rho_initial"
+  #   @debug "Recovered rho:\n$rho_rec" rho_rec
+  #   # @debug "Free rho" rho_free
+  # end
   # rho_initial = state.ρ0
   fid_initial = fidelity(rho_initial, rho_rec)
   fid_track = fidelity(rho_initial, rho_free)
